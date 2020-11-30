c4da3b406c9e98cfdc74ca5490158e75e7a9641c
Franke <frankedeveloper@gmail.com>
2020-11-30 14:35:38 -0300

    Parcial de mostragem de mapa

---
+++ resources/assets/js/components/MapComponent/MapComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="MapComponent">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Mapcomponent'
+    };
+</script>
