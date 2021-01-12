cb3a8008a879251332e681b77d936a28ebd19b2d
Franke <frankedeveloper@gmail.com>
2021-01-12 18:20:31 -0300

    Adiciona nome da empresa na view

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -27,6 +27,12 @@
 
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="javascript:void(0);">
+						<i class="fas fa-house-user"></i>
+                        {{ company()->company_name }}
+                    </a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                         <i class="fas fa-expand-arrows-alt"></i>
