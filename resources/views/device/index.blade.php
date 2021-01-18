a9f1f17e1462a7d84821db20f8f5a57385e80bc2
Franke <frankedeveloper@gmail.com>
2021-01-18 19:01:56 -0300

    ao clicar em cima do dispositivo, ir para detalhe

--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -6,15 +6,19 @@
 )
 @section('content')
     <div class="row">
-        @foreach($devices as $device)
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3>{{ $device->code_device }}</h3>
-                    </div>
-                    <a href="{{ route('device.detail', $device) }}" class="small-box-footer">Detalhes <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
+		@foreach($devices as $device)
+			<div class="col-lg-3 col-6">
+				<div class="small-box bg-success">
+					<a href="{{ route('device.detail', $device) }}" class="block">
+						<div class="inner">
+							<h3>{{ $device->code_device }}</h3>
+						</div>
+						<div  class="small-box-footer">Detalhes
+							<i class="fas fa-arrow-circle-right"></i>
+						</div>
+					</a>
+				</div>
+			</div>
     @endforeach
     </div>
 @stop
