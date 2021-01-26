7478d5453cf5c81525234c94bd433657b12cf957
Franke <frankedeveloper@gmail.com>
2021-01-25 23:34:26 -0300

    Ajusta titulos de paginas

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="base-url" content="{{ url()->to('/') }}">
-    <title>Aztlan | CRM</title>
+    <title>AL2 | LOGIN</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('assets/css/login.min.css')  }}">
 </head>
