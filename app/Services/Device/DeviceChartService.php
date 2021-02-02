16d557e8d05b3aa23d677aa6bc3a1e2f92163696
Franke <frankedeveloper@gmail.com>
2021-02-01 22:55:47 -0300

    Refatora filtragem de gráfico e melhora código

--- app/Services/Device/DeviceChartService.php
+++ app/Services/Device/DeviceChartService.php
@@ -74,10 +74,7 @@ class DeviceChartService
 		$sets = [];
 
 		// Eixo x do dispositivo
-		$fieldToChart = $device->charts->filter(function($chart) {
-			return $chart->id == ChartTypeEnum::LINE;
-		});
-
+		$fieldToChart = $this->returnCharType($device);
 		$fieldToChart = $fieldToChart->first();
 
 		$initialDate = now()->startOfHour();
@@ -87,50 +84,74 @@ class DeviceChartService
 		}
 
 		$finalDate = now()->endOfHour();
+
 		if(!is_null($request->get('finalDate'))) {
 			$finalDate = $request->get('finalDate');
 			$finalDate = Carbon::parse($finalDate);
 		}
 
-
 		if(!empty($fieldToChart->pivot)) {
 			// pegar todos os valores desse campo
-			$pivotId = $fieldToChart->pivot->field_id;
-			$fieldAxe = $device->fields->filter(function($field) use ($pivotId){
-				return $field->id === $pivotId;
-			})->first();
-
-			$dataLabels = $fieldAxe->values()->whereBetween('value', [
-				$initialDate->format('Y-m-d H:i'),
-				$finalDate->format('Y-m-d H:i')
-			])->get();
-
-			$dataLabels = $dataLabels->pluck('formatted_value')->toArray();
+			$xValues = $this->returnXaxeValues($device, $fieldToChart->pivot, $initialDate, $finalDate);
+			$dataLabels = $xValues['dataLabels'];
+			$fieldAxe = $xValues['fieldAxe'];
 
 			$fields = $device->fields->filter(function($field) use ($fieldAxe){
 				return $field->field !== $fieldAxe->field && $field->show_on_chart == TrueOrFalseEnum::TRUE;
 			});
 
-			$sets = [];
-			foreach($fields as $key => $field) {
-				if($field->field !== $fieldAxe->field) {
-					$values = $field->values->filter(function ($value) use ($initialDate, $finalDate){
-						return $value->created_at->between($initialDate->format('Y-m-d H:i'), $finalDate->format('Y-m-d H:i'));
-					});
-
-					$sets[] = [
-						'label' => $field->list_name,
-						'backgroundColor' => $field->color_on_chart,
-						'data' => $values->pluck('value')->toArray()
-					];
-				}
-			}
+			$sets = $this->returnSetValeusYAxe($fields, $fieldAxe, $initialDate, $finalDate);
 		}
 
 		return collect([
 			'labels' => $dataLabels,
 			'sets' => $sets
 		]);
+	}
+
+	private function returnSetValeusYAxe($fields, $fieldAxe, $initialDate, $finalDate)
+	{
+		$sets = [];
+		foreach($fields as $key => $field) {
+			if($field->field !== $fieldAxe->field) {
+				$values = $field->values->filter(function ($value) use ($initialDate, $finalDate){
+					return $value->created_at->between($initialDate->format('Y-m-d H:i'), $finalDate->format('Y-m-d H:i'));
+				});
+
+				$sets[] = [
+					'label' => $field->list_name,
+					'backgroundColor' => $field->color_on_chart,
+					'data' => $values->pluck('value')->toArray()
+				];
+			}
+		}
+
+		return $sets;
+	}
+
+	private function returnXaxeValues($device, $pivot, $initialDate, $finalDate)
+	{
+		$pivotId = $pivot->field_id;
+		$fieldAxe = $device->fields->filter(function($field) use ($pivotId){
+			return $field->id === $pivotId;
+		})->first();
+
+		$dataLabels = $fieldAxe->values()->whereBetween('created_at', [
+			$initialDate->format('Y-m-d H:i'),
+			$finalDate->format('Y-m-d H:i')
+		])->get();
+
+		return [
+			'dataLabels' => $dataLabels->pluck('formatted_value')->toArray(),
+			'fieldAxe' => $fieldAxe
+		];
+	}
+
+	private function returnCharType(Device $device)
+	{
+		return $device->charts->filter(function($chart) {
+			return $chart->id == ChartTypeEnum::LINE;
+		});
 	}
 
 }
