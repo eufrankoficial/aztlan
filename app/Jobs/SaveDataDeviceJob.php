e11bfdd4e46672465f9a07e77dcc695f5a012121
Franke <frankedeveloper@gmail.com>
2021-03-10 11:51:56 -0300

    Cria um novo job na falha de outro

--- app/Jobs/SaveDataDeviceJob.php
+++ app/Jobs/SaveDataDeviceJob.php
@@ -4,52 +4,55 @@ namespace App\Jobs;
 
 use App\Services\Device\DeviceService;
 use App\Services\System\FieldService;
+use App\Services\System\JobService;
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SaveDataDeviceJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     private $deviceData;
     private $deviceService;
     private $fieldService;
+    private $jobService;
 
     /**
      * Create a new job instance.
-     *
-     * @return void
      */
     public function __construct(array $deviceData)
     {
         $this->deviceData = $deviceData;
-		$this->deviceService = app(DeviceService::class);
-		$this->fieldService = app(FieldService::class);
+        $this->deviceService = app(DeviceService::class);
+        $this->fieldService = app(FieldService::class);
+        $this->jobService = app(JobService::class);
     }
 
     /**
      * Execute the job.
-     *
-     * @return void
      */
     public function handle()
     {
-    	try {
-    		DB::beginTransaction();
-			$device = $this->deviceService->save([
-				'code_device' => $this->deviceData['id']
-			]);
-			$this->fieldService->prepareFieldsAndSave($this->deviceData, $device);
-			DB::commit();
-		} catch (\Exception $e) {
-			DB::rollBack();
-			Log::error($e->getMessage());
-    		Log::error('Error on job: . ' . json_encode($this->deviceData));
-		}
+        try {
+            DB::beginTransaction();
+            $device = $this->deviceService->save([
+                'code_device' => $this->deviceData['id'],
+            ]);
+            $this->fieldService->prepareFieldsAndSave($this->deviceData, $device);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            Log::error('Error on job: . '.json_encode($this->deviceData));
+            $this->jobService->createJobOnQueue($this->deviceData);
+        }
     }
 }
