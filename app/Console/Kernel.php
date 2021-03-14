570ea5de871a53efc5f91f5e91bde4e1ab26a2ce
Franke <frankedeveloper@gmail.com>
2021-03-14 20:20:24 -0300

    Roda cron a cada cinco minutos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:read-device-tmp-table')->everyMinute();
+        $schedule->command('command:read-device-tmp-table')->everyFiveMinutes();
     }
 
     /**
